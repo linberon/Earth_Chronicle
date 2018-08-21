df470467f27ca91f3dde7843dcf1b4da5e57e55b
linberon <linberon@gmail.com>
2018-08-22 00:44:44 +0100

    first template

--- app.js
+++ app.js
@@ -1,7 +1,50 @@
 Vue.component("earth-page", {
+	template: `
+	<div>
+		<p>
+			The lithosphere, which is the rigid outermost shell of a planet, is broken into tectonic plates. 
+			The Earth's lithosphere is composed of seven or eight major plates and many minor plates. 
+			Tectonic motion is believed to have begun around 3 to 3.5 billion years ago.
+		</p>
 
+		<p>
+			The movement of plates has caused the formation and break-up of continents over time, 
+			including occasional formation of a supercontinent that contains most or all of the continents.
+		</p>
+
+		<p>
+			Plate motions range up to a typical 10–40 mm/year (Mid-Atlantic Ridge; about as fast as 
+			fingernails grow), to about 160 mm/year (Nazca Plate; about as fast as hair grows).
+		</p>
+
+		<p>
+			The location where two plates meet is called a plate boundary. Plate boundaries are commonly 
+			associated with earthquakes and the creation of mountains, volcanoes, mid-ocean ridges, and 
+			oceanic trenches. 
+		</p>
+
+		<p>
+			The majority of the world's active volcanoes occur along plate boundaries, with the 
+			Pacific Plate's Ring of Fire being the most active and widely known today.
+		</p>
+
+		<p>
+			When plates collide or undergo subduction (that is – ride one over another), the plates tend to 
+			buckle and fold, forming mountains. 
+		</p>
+
+		<p>
+			The Himalayas, the world's tallest mountain range, are assumed to have been formed by the 
+			collision of two major plates. Before uplift, they were covered by the Tethys Ocean.
+		</p>
+
+		<p>
+			The current motion of the tectonic plates is today determined by remote sensing satellite data 
+			sets, calibrated with ground station measurements.
+		</p>
+	</div>`
 });
 
-new Vue ({
+new Vue({
 	el: "#app"
-}),
+});
